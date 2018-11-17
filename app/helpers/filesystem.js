8303c20e823aab1eb8d9105f962f381745ef581d
Rodrigo Ramirez <xpromx@gmail.com>
2018-11-18 00:58:36 +0900

    S3 and Local drive

--- app/helpers/filesystem.js
+++ app/helpers/filesystem.js
@@ -4,6 +4,37 @@ import axios from 'axios'
 import moment from 'moment'
 
 class Drive {
+  constructor () {}
+  upload = (path, stream, bucket = null) => {}
+  uploadFromUrl = async (path, url, bucket = null) => {
+    const res = await axios(url, {
+      responseType: 'arraybuffer'
+    })
+
+    return this.upload(path, res.data)
+  }
+  makeThumb = async (path, stream, w, h) => {
+    const data = await sharp(stream).resize(w, h).toBuffer()
+    return this.upload(path, data)
+  }
+
+  getPath = (filename, date = null) => {
+    const dateFormat = date ? moment(date) : moment()
+    const year = dateFormat.format('Y')
+    const month = dateFormat.format('MM')
+
+    return `${year}\/${month}\/${filename}`
+  }
+
+  getFileExt = (filename) => {
+    const parts = filename.split('.')
+    return parts[parts.length - 1].toLowerCase()
+  }
+
+  upload = (path, stream, bucket = null) => {}
+}
+
+class DriveS3 extends Drive {
   constructor () {
     AWS.config.update({
       accessKeyId: process.env.S3_KEY,
@@ -15,14 +46,6 @@ class Drive {
       basePath: process.env.S3_PATH ? process.env.S3_PATH + '/' : '',
       bucket: process.env.S3_BUCKET
     }
-  }
-
-  uploadFromUrl = async (path, url, bucket = null) => {
-    const res = await axios(url, {
-      responseType: 'arraybuffer'
-    })
-
-    return this.upload(path, res.data)
   }
 
   upload = (path, stream, bucket = null) => {
@@ -43,29 +66,15 @@ class Drive {
     })
   }
 
-  getPath = (filename, date = null) => {
-    const dateFormat = date ? moment(date) : moment()
-    const year = dateFormat.format('Y')
-    const month = dateFormat.format('MM')
-
-    return `${year}\/${month}\/${filename}`
-  }
-
-  getFileExt = (filename) => {
-    const parts = filename.split('.')
-    return parts[parts.length - 1].toLowerCase()
-  }
-
   getUrl = (path) => {
     return process.env.S3_URL + path
   }
-
-  makeThumb = async (path, stream, w, h) => {
-    const data = await sharp(stream).resize(w, h).toBuffer()
-    return this.upload(path, data)
-  }
 }
 
-const S3 = new Drive()
+const Drive = new Drive()
+Drive.S3 = new DriveS3()
+
+// Drive.S3.upload()
+// Drive.upload()
 
 export default S3
